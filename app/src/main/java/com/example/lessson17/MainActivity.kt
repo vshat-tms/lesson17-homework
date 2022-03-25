a00a470d02e2e8d99d3428ca03094ed239dfd20a
PolichF1 <polichsegg@gmail.com>
2022-03-25 22:15:37 +0300

    Update code (no double findId)

--- app/src/main/java/com/example/lessson17/MainActivity.kt
+++ app/src/main/java/com/example/lessson17/MainActivity.kt
@@ -3,127 +3,164 @@ package com.example.lessson17
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
+import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import java.text.SimpleDateFormat
 import java.time.LocalTime
+import java.util.*
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var imageView: ImageView
-    var counter = 0
+    private lateinit var displayTextView: TextView
+    private lateinit var imageView: ImageView
+    private lateinit var linearLayout: LinearLayout
+    private var counter = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        findViewById<Button>(R.id.plusButton).setOnClickListener{
-            findViewById<TextView>(R.id.infoField).text = "${++counter}"
-        }
-
-        findViewById<Button>(R.id.minusButton).setOnClickListener{
-            findViewById<TextView>(R.id.infoField).text = "${--counter}"
-        }
-
-        findViewById<Button>(R.id.randomNumberButton).setOnClickListener{
-            counter = Random.nextInt(100)
-            findViewById<TextView>(R.id.infoField).text = "$counter"
-        }
-
-        findViewById<Button>(R.id.toZeroButton).setOnClickListener{
-            counter = 0
-            findViewById<TextView>(R.id.infoField).text = "$counter"
-        }
-
-
-
-        findViewById<Button>(R.id.showDeviceInfoButton).setOnClickListener{
-            findViewById<TextView>(R.id.infoField).text = "${Build.BRAND} ${Build.MODEL}"
-        }
-
-        findViewById<Button>(R.id.showCurrentTimeButton).setOnClickListener {
-            val nowTime = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                LocalTime.now()
-            } else {
-                TODO("VERSION.SDK_INT < O")
-            }
-            findViewById<TextView>(R.id.infoField).text = "$nowTime"
-        }
-
-        findViewById<Button>(R.id.toastButton).setOnClickListener{
-            val text = "TOAST"
-            val duration = Toast.LENGTH_SHORT
-            val toast = Toast.makeText(this, text, duration)
-            toast.show()
-        }
-
-
-
-        findViewById<Button>(R.id.setTextColorRedButton).setOnClickListener{
-            findViewById<TextView>(R.id.infoField).setTextColor(Color.parseColor("red"))
-        }
-
-        findViewById<Button>(R.id.setTextColorGreenButton).setOnClickListener{
-            findViewById<TextView>(R.id.infoField).setTextColor(Color.parseColor("green"))
-        }
-
-        findViewById<Button>(R.id.setTextColorBlueButton).setOnClickListener{
-            findViewById<TextView>(R.id.infoField).setTextColor(Color.parseColor("blue"))
-        }
-
-        findViewById<Button>(R.id.setTextColorMagentaButton).setOnClickListener{
-            findViewById<TextView>(R.id.infoField).setTextColor(Color.parseColor("magenta"))
-        }
-
-
-
-        findViewById<Button>(R.id.setBackgroundColorBlack).setOnClickListener{
-            findViewById<LinearLayout>(R.id.background).setBackgroundColor(Color.parseColor("Black"))
-        }
-
-        findViewById<Button>(R.id.setBackgroundColorYellow).setOnClickListener{
-                findViewById<LinearLayout>(R.id.background).setBackgroundColor(Color.parseColor("Yellow"))
-        }
-
-        findViewById<Button>(R.id.setBackgroundColorGray).setOnClickListener{
-            findViewById<LinearLayout>(R.id.background).setBackgroundColor(Color.parseColor("Gray"))
-        }
-
-        findViewById<Button>(R.id.setBackgroundColorCyan).setOnClickListener {
-            findViewById<LinearLayout>(R.id.background).setBackgroundColor(Color.parseColor("Cyan"))
-        }
-
-
-
-        imageView = findViewById(R.id.image)
-        imageView.setOnClickListener { imageView.rotation = 90F }
-
-        findViewById<Button>(R.id.setCatImage).setOnClickListener{
-            findViewById<ImageView>(R.id.image).animate().rotation(0F)
-            findViewById<ImageView>(R.id.image).setImageResource(R.drawable.cat)
-        }
-
-        findViewById<Button>(R.id.setDogImage).setOnClickListener{
-            findViewById<ImageView>(R.id.image).animate().rotation(0F)
-            findViewById<ImageView>(R.id.image).setImageResource(R.drawable.dog)
-        }
-
-        findViewById<Button>(R.id.setParrotImage).setOnClickListener{
-            findViewById<ImageView>(R.id.image).animate().rotation(0F)
-            findViewById<ImageView>(R.id.image).setImageResource(R.drawable.parrot)
-        }
-
-        findViewById<Button>(R.id.setRandomImage).setOnClickListener {
-            findViewById<ImageView>(R.id.image).animate().rotation(0F)
-            when(Random.nextInt(4)){
-                1 -> findViewById<ImageView>(R.id.image).setImageResource(R.drawable.cat)
-                2 -> findViewById<ImageView>(R.id.image).setImageResource(R.drawable.dog)
-                3 -> findViewById<ImageView>(R.id.image).setImageResource(R.drawable.parrot)
-            }
-        }
-
-
-
-
+        displayTextView = findViewById(R.id.displayTextView)
+        imageView = findViewById(R.id.imageView)
+        linearLayout = findViewById(R.id.background)
     }
+
+    fun onButtonClick(view: View){
+        when(view.id){
+            R.id.plusButton -> counterPlusButtonClick()
+            R.id.minusButton -> counterMinusButtonClick()
+            R.id.randomNumberButton -> counterRandomButtonClick()
+            R.id.toZeroButton -> counterToZeroButtonClick()
+            R.id.showDeviceInfoButton -> infoShowDeviceModelButton()
+            R.id.showCurrentTimeButton -> infoShowTimeButtonClick()
+            R.id.toastButton -> infoShowToastButtonClick()
+            R.id.setTextColorRedButton -> colorTextSetRedButtonClick()
+            R.id.setTextColorGreenButton -> colorTextSetGreenButtonClick()
+            R.id.setTextColorBlueButton -> colorTextSetBlueButtonClick()
+            R.id.setTextColorMagentaButton -> colorTextSetMagentaButtonClick()
+            R.id.setBackgroundColorBlack -> backgroundColorSetBlackButtonClick()
+            R.id.setBackgroundColorYellow -> backgroundColorSetYellowButtonClick()
+            R.id.setBackgroundColorGray -> backgroundColorSetGrayButtonClick()
+            R.id.setBackgroundColorCyan -> backgroundColorSetCyanButtonClick()
+            R.id.setCatImage -> imageSetCatButtonClick()
+            R.id.setDogImage -> imageSetDogButtonClick()
+            R.id.setParrotImage -> imageSetParrotButtonClick()
+            R.id.setRandomImage -> imageSetRandomButtonClick()
+            R.id.imageView -> imageToRotateButtonClick()
+        }
+    }
+
+    private fun counterPlusButtonClick(){
+        displayTextView.text = "${++counter}"
+    }
+
+    private fun counterMinusButtonClick(){
+        displayTextView.text = "${--counter}"
+    }
+
+    private fun counterRandomButtonClick(){
+        counter = Random.nextInt(100)
+        displayTextView.text = "$counter"
+    }
+
+    private fun counterToZeroButtonClick(){
+        counter = 0
+        displayTextView.text = "$counter"
+    }
+
+    private fun infoShowDeviceModelButton(){
+        displayTextView.text = "${Build.MODEL}"
+    }
+
+    private fun infoShowTimeButtonClick(){
+        displayTextView.text = "$CURRENT_TIME"
+    }
+
+    private fun infoShowToastButtonClick(){
+        val toast = Toast.makeText(this, TOAST_TEXT, Toast.LENGTH_LONG)
+        toast.show()
+    }
+
+    private fun colorTextSetRedButtonClick(){
+        displayTextView.setTextColor(Color.RED)
+    }
+
+    private fun colorTextSetGreenButtonClick(){
+        displayTextView.setTextColor(Color.GREEN)
+    }
+
+    private fun colorTextSetBlueButtonClick(){
+        displayTextView.setTextColor(Color.BLUE)
+    }
+
+    private fun colorTextSetMagentaButtonClick(){
+        displayTextView.setTextColor(Color.MAGENTA)
+    }
+
+    private fun backgroundColorSetBlackButtonClick(){
+        linearLayout.setBackgroundColor(Color.BLACK)
+    }
+
+    private fun backgroundColorSetYellowButtonClick(){
+        linearLayout.setBackgroundColor(Color.YELLOW)
+    }
+
+    private fun backgroundColorSetGrayButtonClick(){
+        linearLayout.setBackgroundColor(Color.GRAY)
+    }
+
+    private fun backgroundColorSetCyanButtonClick(){
+        linearLayout.setBackgroundColor(Color.CYAN)
+    }
+
+    private fun setDefaultRotationToImage(){
+        imageView.rotation = ROTATION_DEGREES.first()
+    }
+
+    private fun imageToRotateButtonClick(){
+        val currentRotation = imageView.rotation
+        val index = ROTATION_DEGREES.indexOf(currentRotation)
+        if (currentRotation < ROTATION_DEGREES.last()){
+            imageView.rotation = ROTATION_DEGREES[index + 1]
+        }else{
+            imageView.rotation = ROTATION_DEGREES.first()
+        }
+    }
+
+    private fun imageSetCatButtonClick(){
+        setDefaultRotationToImage()
+        imageView.setImageResource(R.drawable.cat)
+    }
+
+    private fun imageSetDogButtonClick(){
+        setDefaultRotationToImage()
+        imageView.setImageResource(R.drawable.dog)
+    }
+
+    private fun imageSetParrotButtonClick(){
+        setDefaultRotationToImage()
+        imageView.setImageResource(R.drawable.parrot)
+    }
+
+    private fun imageSetRandomButtonClick(){
+        setDefaultRotationToImage()
+        imageView.setImageResource(
+            PICTURES_ID[Random.nextInt(PICTURES_ID.size)]
+        )
+    }
+
+
+
+
+
+    companion object{
+        private val DATE_FORMAT = SimpleDateFormat("HH:mm:ss")
+        private val CURRENT_TIME: String = DATE_FORMAT.format(Date())
+        private const val TOAST_TEXT = "Just message"
+        private val ROTATION_DEGREES = listOf(0F, 90F, 180F, 270F)
+        private val PICTURES_ID = listOf(R.drawable.cat, R.drawable.dog, R.drawable.parrot)
+    }
+
+
 }
