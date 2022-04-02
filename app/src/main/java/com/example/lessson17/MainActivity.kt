9303154cc3be79567f3b6ae1c34f9598b91ba4bb
Ulasevich Vadim <ulasevichvadimsergeevich@yandex.ru>
2022-04-02 17:43:06 +0300

    lesson 17 Ulasevich

--- app/src/main/java/com/example/lessson17/MainActivity.kt
+++ app/src/main/java/com/example/lessson17/MainActivity.kt
@@ -6,7 +6,8 @@ import android.os.Bundle
 import android.view.Gravity
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import java.time.LocalTime
+import java.text.DateFormat
+import java.util.*
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
@@ -18,82 +19,80 @@ class MainActivity : AppCompatActivity() {
         var count = 0
         val screenTable = findViewById<TextView>(R.id.screen)
         val background = findViewById<LinearLayout>(R.id.background)
-        val image =  findViewById<ImageView>(R.id.picture)
+        val image = findViewById<ImageView>(R.id.picture)
 
-        findViewById<Button>(R.id.plusButton).setOnClickListener{
-            screenTable.text="${count++}"
+        findViewById<Button>(R.id.plusButton).setOnClickListener {
+            screenTable.text = "${count++}"
         }
-        findViewById<Button>(R.id.minusButton).setOnClickListener{
-            screenTable.text="${count--}"
+        findViewById<Button>(R.id.minusButton).setOnClickListener {
+            screenTable.text = "${count--}"
         }
-        findViewById<Button>(R.id.zeroButton).setOnClickListener{
+        findViewById<Button>(R.id.zeroButton).setOnClickListener {
             count = 0
-            screenTable.text= "$count"
+            screenTable.text = "$count"
         }
-        findViewById<Button>(R.id.randomButton).setOnClickListener{
+        findViewById<Button>(R.id.randomButton).setOnClickListener {
             val random = Random.nextInt(RANDOM_MIN_VALUE, RANDOM_MAX_VALUE)
-            screenTable.text="$random"
+            screenTable.text = "$random"
         }
-        findViewById<Button>(R.id.rColorButton).setOnClickListener{
+        findViewById<Button>(R.id.rColorButton).setOnClickListener {
             screenTable.setTextColor(Color.parseColor("red"))
         }
-        findViewById<Button>(R.id.gColorButton).setOnClickListener{
+        findViewById<Button>(R.id.gColorButton).setOnClickListener {
             screenTable.setTextColor(Color.parseColor("green"))
         }
-        findViewById<Button>(R.id.bColorButton).setOnClickListener{
+        findViewById<Button>(R.id.bColorButton).setOnClickListener {
             screenTable.setTextColor(Color.parseColor("blue"))
         }
-        findViewById<Button>(R.id.mColorButton).setOnClickListener{
+        findViewById<Button>(R.id.mColorButton).setOnClickListener {
             screenTable.setTextColor(Color.parseColor("magenta"))
         }
-        findViewById<Button>(R.id.oneBackgroundButton).setOnClickListener{
+        findViewById<Button>(R.id.oneBackgroundButton).setOnClickListener {
             background.setBackgroundResource(R.drawable.ground)
         }
-        findViewById<Button>(R.id.twoBackgroundButton).setOnClickListener{
+        findViewById<Button>(R.id.twoBackgroundButton).setOnClickListener {
             background.setBackgroundResource(R.drawable.ground2)
         }
-        findViewById<Button>(R.id.threeBackgroundButton).setOnClickListener{
+        findViewById<Button>(R.id.threeBackgroundButton).setOnClickListener {
             background.setBackgroundResource(R.drawable.ground3)
         }
-        findViewById<Button>(R.id.fourBackgroundButton).setOnClickListener{
+        findViewById<Button>(R.id.fourBackgroundButton).setOnClickListener {
             background.setBackgroundResource(R.drawable.ground4)
         }
-        findViewById<Button>(R.id.catButton).setOnClickListener{
+        findViewById<Button>(R.id.catButton).setOnClickListener {
             image.animate().rotation(0F)
             image.setImageResource(R.drawable.cat)
         }
-        findViewById<Button>(R.id.dogButton).setOnClickListener{
+        findViewById<Button>(R.id.dogButton).setOnClickListener {
             image.animate().rotation(0F)
             image.setImageResource(R.drawable.dog)
         }
-        findViewById<Button>(R.id.parrotButton).setOnClickListener{
+        findViewById<Button>(R.id.parrotButton).setOnClickListener {
             image.animate().rotation(0F)
             image.setImageResource(R.drawable.parrot)
         }
-        findViewById<Button>(R.id.randomPictureButton).setOnClickListener{
+        findViewById<Button>(R.id.randomPictureButton).setOnClickListener {
             image.animate().rotation(0F)
-            when(Random.nextInt(1,4)){
+            when (Random.nextInt(1, 4)) {
                 1 -> image.setImageResource(R.drawable.cat)
                 2 -> image.setImageResource(R.drawable.dog)
                 3 -> image.setImageResource(R.drawable.parrot)
             }
         }
-        findViewById<Button>(R.id.deviceButton).setOnClickListener{
+        findViewById<Button>(R.id.deviceButton).setOnClickListener {
             screenTable.text = "${Build.BRAND} ${Build.DISPLAY}"
         }
-        findViewById<Button>(R.id.timeButton).setOnClickListener{
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                screenTable.text = "${LocalTime.now().hour}:${LocalTime.now().minute}"
-            }
+        findViewById<Button>(R.id.timeButton).setOnClickListener {
+            screenTable.text = DateFormat.getTimeInstance(DateFormat.SHORT).format(Date())
         }
-        findViewById<Button>(R.id.toastButton).setOnClickListener{
+        findViewById<Button>(R.id.toastButton).setOnClickListener {
             val text = "You entered button TOAST!"
             val duration = Toast.LENGTH_SHORT
             val toast = Toast.makeText(this, text, duration)
             toast.setGravity(Gravity.CENTER, 0, 0)
             toast.show()
         }
-        image.setOnClickListener{
+        image.setOnClickListener {
             when (image.rotation) {
                 0F -> image.animate().rotation(90f)
                 90F -> image.animate().rotation(180f)
@@ -103,7 +102,8 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-    companion object{
+
+    companion object {
         private const val RANDOM_MIN_VALUE = -100
         private const val RANDOM_MAX_VALUE = 100
     }
